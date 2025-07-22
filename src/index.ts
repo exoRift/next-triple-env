a3d12ba4a4541fbbe6f74f39b0ca1427ddb22b26
Arthur Neuman <arthurbneuman@gmail.com>
2025-07-22 13:13:26 -0400

    Switch from global to globalThis

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,5 @@ const TShared extends Record<`NEXT_PLUBLIC_${string}`, z.ZodType> = {},
 const TStatic extends Record<`NEXT_STATIC_${string}`, z.ZodType> = {}
 > (...params: ConstructorParameters<typeof ValidatedEnvironment<TServer, TShared, TStatic>>): ValidatedEnvironment<TServer, TShared, TStatic> {
   // @ts-expect-error -- We want this to be a singleton and we want the automatic type inference so we don't declare its type on global
-  return ((global._env as undefined) ??= new ValidatedEnvironment(...params)) /* eslint-disable-line @typescript-eslint/no-unnecessary-condition */
+  return ((globalThis._env as undefined) ??= new ValidatedEnvironment(...params)) /* eslint-disable-line @typescript-eslint/no-unnecessary-condition */
 }
